a360f4a75307d393a95ae28127e0ebb90dcd8322
EdenilsonBila <edn_santos@hotmail.com>
2022-05-14 11:33:43 -0300

    Add project files.

---
+++ Formula1/Formula1/Views/DriverDetailsView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Formula1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Formula1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DriverDetailsView : ContentPage
+    {
+        public DriverDetailsView(Driver model)
+        {
+            InitializeComponent();
+
+            this.BindingContext = model;
+        }
+    }
+}
---
+++ Formula1/Formula1/Views/DriversView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Formula1.Data;
+using Formula1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Formula1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DriversView : ContentPage
+    {
+        private DriverRepository driverRepository;
+        public DriversView()
+        {
+            InitializeComponent();
+            driverRepository = new DriverRepository();
+            DriversList.ItemsSource = driverRepository.FindAll();
+        }
+
+        private void DriversList_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            //Obtem o item clicado na tela
+            Driver driverSelected = e.Item as Driver;
+            Navigation.PushAsync(new DriverDetailsView(driverSelected));
+        }
+    }
+}
